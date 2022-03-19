7afbd37d1a120ed40857ddf4a6613ac3cdaf40af
namduongdev <namduong.dev@gmail.com>
2022-03-19 15:42:40 +0700

    <intent-filter>

---
+++ app/src/main/java/com/tutuanle/chatapp/PhoneNumberActivity.java
@@ -0,0 +1,14 @@
+package com.tutuanle.chatapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PhoneNumberActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_phone_number);
+    }
+}
