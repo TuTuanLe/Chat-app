0c61092a3aa79a7c7369512c9137574f829cabfa
TuTuanLe <tutuanletkpt@gmail.com>
2022-03-20 20:06:45 +0700

    add model user

---
+++ app/src/main/java/com/tutuanle/chatapp/model/User.java
@@ -0,0 +1,47 @@
+package com.tutuanle.chatapp.model;
+
+public class User {
+    private  String uid, name, phoneNumber,profileImage;
+
+    public User(String uid, String name, String phoneNumber, String profileImage) {
+        this.uid = uid;
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.profileImage = profileImage;
+    }
+
+    public User() {
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public void setProfileImage(String profileImage) {
+        this.profileImage = profileImage;
+    }
+
+    public String getUid() {
+        return uid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public String getProfileImage() {
+        return profileImage;
+    }
+}
