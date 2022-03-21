a1e384c9745c53e7bb7073d224ad5a688296d017
TuTuanLe <tutuanletkpt@gmail.com>
2022-03-21 10:30:18 +0700

    add page Setup profile

---
+++ app/src/main/java/com/tutuanle/chatapp/SetupProfileActivity.java
@@ -0,0 +1,51 @@
+package com.tutuanle.chatapp;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.tutuanle.chatapp.databinding.ActivitySetupProfileBinding;
+
+
+public class SetupProfileActivity extends AppCompatActivity {
+    ActivitySetupProfileBinding binding;
+    FirebaseAuth auth;
+    FirebaseDatabase database;
+    FirebaseStorage storage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivitySetupProfileBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        database = FirebaseDatabase.getInstance();
+        storage = FirebaseStorage.getInstance();
+        auth = FirebaseAuth.getInstance();
+        binding.imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                intent.setType("image/*");
+                startActivityForResult(intent, 45);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (data != null) {
+            if (data.getData() != null){
+                binding.imageView.setImageURI(data.getData());
+            }
+        }
+    }
+}
