f0fbaccfbf00114c74aa151335181ad605fa3ffd
TuTuanLe <tutuanletkpt@gmail.com>
2022-03-21 11:31:27 +0700

    add navigation page

--- app/src/main/java/com/tutuanle/chatapp/PhoneNumberActivity.java
+++ app/src/main/java/com/tutuanle/chatapp/PhoneNumberActivity.java
@@ -6,20 +6,31 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.tutuanle.chatapp.databinding.ActivityPhoneNumberBinding;
 
 public class PhoneNumberActivity extends AppCompatActivity {
 
     ActivityPhoneNumberBinding binding;
-
+    FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityPhoneNumberBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        auth = FirebaseAuth.getInstance();
+         if(auth.getCurrentUser()!= null){
+             Intent intent = new Intent(PhoneNumberActivity.this, MainActivity.class);
+             startActivity(intent);
+             finish();
+         }
+
         getSupportActionBar().hide();
         binding.phoneBox.requestFocus();
+
+
 
         binding.continueBtn.setOnClickListener(new View.OnClickListener() {
             @Override
