94d4c277b28aef6a308457cf325f6731fff7b588
TuTuanLe <tutuanletkpt@gmail.com>
2022-03-22 08:42:43 +0700

    refactor row user

--- app/src/main/java/com/tutuanle/chatapp/MainActivity.java
+++ app/src/main/java/com/tutuanle/chatapp/MainActivity.java
@@ -2,18 +2,60 @@ package com.tutuanle.chatapp;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.tutuanle.chatapp.adapter.UsersAdapter;
+import com.tutuanle.chatapp.databinding.ActivityMainBinding;
+import com.tutuanle.chatapp.model.User;
+
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+    ActivityMainBinding binding;
+    FirebaseDatabase database;
+    ArrayList<User> users;
+    UsersAdapter usersAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        database = FirebaseDatabase.getInstance();
+        users = new ArrayList<User>();
+        usersAdapter= new UsersAdapter(this, users);
+//        binding.recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        binding.recyclerView.setAdapter(usersAdapter);
+
+        database.getReference().child("users").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                users.clear();
+                for(DataSnapshot snapshot1 : snapshot.getChildren()){
+
+                    User user = snapshot1.getValue(User.class);
+                    Log.d("log",user.toString());
+//                    users.add(user);
+                }
+//                usersAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
     }
 
     @Override
---
+++ app/src/main/java/com/tutuanle/chatapp/adapter/UsersAdapter.java
@@ -0,0 +1,53 @@
+package com.tutuanle.chatapp.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.tutuanle.chatapp.R;
+import com.tutuanle.chatapp.databinding.RowConversationBinding;
+import com.tutuanle.chatapp.model.User;
+
+import java.util.ArrayList;
+
+public class UsersAdapter extends RecyclerView.Adapter<UsersAdapter.UsersViewHolder> {
+    Context context;
+    ArrayList<User> users;
+
+    public UsersAdapter(Context context, ArrayList<User> users) {
+        this.context = context;
+        this.users = users;
+    }
+
+    @NonNull
+    @Override
+    public UsersViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.row_conversation, parent, false);
+
+        return new UsersViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull UsersViewHolder holder, int position) {
+        User user = users.get(position);
+        holder.binding.username.setText(user.getName());
+    }
+
+    @Override
+    public int getItemCount() {
+        return users.size();
+    }
+
+    public class UsersViewHolder extends RecyclerView.ViewHolder {
+        RowConversationBinding binding;
+
+
+        public UsersViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+}
