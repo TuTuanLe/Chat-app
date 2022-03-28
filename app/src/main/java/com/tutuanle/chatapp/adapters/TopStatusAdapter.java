b0d5e156511e2deecf10a7280a77e1d8d7b827b6
TuTuanLe <tutuanletkpt@gmail.com>
2022-03-28 15:44:56 +0700

    update

--- app/src/main/java/com/tutuanle/chatapp/adapters/TopStatusAdapter.java
+++ app/src/main/java/com/tutuanle/chatapp/adapters/TopStatusAdapter.java
@@ -9,19 +9,25 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.tutuanle.chatapp.R;
+import com.tutuanle.chatapp.activities.MainActivity;
 import com.tutuanle.chatapp.databinding.ItemStatusBinding;
+import com.tutuanle.chatapp.models.Status;
 import com.tutuanle.chatapp.models.UserStatus;
 
 import java.util.ArrayList;
 
-public class TopStatusAdapter  extends   RecyclerView.Adapter<TopStatusAdapter.TopStatusViewHolder> {
+import omari.hamza.storyview.StoryView;
+import omari.hamza.storyview.callback.StoryClickListeners;
+import omari.hamza.storyview.model.MyStory;
+
+public class TopStatusAdapter extends RecyclerView.Adapter<TopStatusAdapter.TopStatusViewHolder> {
 
     Context context;
     ArrayList<UserStatus> userStatuses;
 
-    public  TopStatusAdapter(Context context, ArrayList<UserStatus> userStatuses){
-        this.context= context;
-        this.userStatuses= userStatuses;
+    public TopStatusAdapter(Context context, ArrayList<UserStatus> userStatuses) {
+        this.context = context;
+        this.userStatuses = userStatuses;
     }
 
     @NonNull
@@ -34,6 +40,37 @@ public class TopStatusAdapter  extends   RecyclerView.Adapter<TopStatusAdapter.T
 
     @Override
     public void onBindViewHolder(@NonNull TopStatusViewHolder holder, int position) {
+        UserStatus userStatus = userStatuses.get(position);
+
+        holder.binding.circularStatusView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ArrayList<MyStory> myStories = new ArrayList<>();
+                for (Status status : userStatus.getStatuses()) {
+                    myStories.add(new MyStory(status.getImageUrl()));
+                }
+                new StoryView.Builder(((MainActivity)context).getSupportFragmentManager())
+                        .setStoriesList(myStories) // Required
+                        .setStoryDuration(5000) // Default is 2000 Millis (2 Seconds)
+                        .setTitleText(userStatus.getName()) // Default is Hidden
+                        .setSubtitleText("") // Default is Hidden
+                        .setTitleLogoUrl(userStatus.getProfileImage()) // Default is Hidden
+                        .setStoryClickListeners(new StoryClickListeners() {
+                            @Override
+                            public void onDescriptionClickListener(int position) {
+                                //your action
+                            }
+
+                            @Override
+                            public void onTitleIconClickListener(int position) {
+                                //your action
+                            }
+                        }) // Optional Listeners
+                        .build() // Must be called before calling show method
+                        .show();
+
+            }
+        });
 
     }
 
@@ -42,8 +79,9 @@ public class TopStatusAdapter  extends   RecyclerView.Adapter<TopStatusAdapter.T
         return userStatuses.size();
     }
 
-    public class TopStatusViewHolder extends RecyclerView.ViewHolder{
+    public class TopStatusViewHolder extends RecyclerView.ViewHolder {
         ItemStatusBinding binding;
+
         public TopStatusViewHolder(@NonNull View itemView) {
             super(itemView);
             binding = ItemStatusBinding.bind(itemView);
