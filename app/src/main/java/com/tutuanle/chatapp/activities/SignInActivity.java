118f381c2232f993a3aafbd44a1dd041960d5d59
TuTuanLe <tutuanletkpt@gmail.com>
2022-04-21 15:50:35 +0700

    update

--- app/src/main/java/com/tutuanle/chatapp/activities/SignInActivity.java
+++ app/src/main/java/com/tutuanle/chatapp/activities/SignInActivity.java
@@ -4,16 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 
+import com.google.firebase.firestore.FirebaseFirestore;
 import com.tutuanle.chatapp.databinding.ActivitySignInBinding;
+
+import java.util.HashMap;
 
 public class SignInActivity extends AppCompatActivity {
 
     FirebaseAuth auth;
 
     private ActivitySignInBinding binding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,8 +27,28 @@ public class SignInActivity extends AppCompatActivity {
         setListeners();
     }
 
-    private void setListeners(){
-        binding.textCreateNewAccount.setOnClickListener( v ->
-                startActivity( new Intent(getApplicationContext(), SignUpActivity.class)));
+    private void setListeners() {
+        binding.textCreateNewAccount.setOnClickListener(v ->
+                startActivity(new Intent(getApplicationContext(), SignUpActivity.class)));
+
+        binding.buttonSignIn.setOnClickListener(v ->
+                addDataToFireStore()
+        );
+    }
+
+    private void addDataToFireStore() {
+        Toast.makeText(getApplicationContext(),"hello", Toast.LENGTH_SHORT).show();
+        FirebaseFirestore database = FirebaseFirestore.getInstance();
+        HashMap<String, Object> data = new HashMap<>();
+        data.put("first_name", "tutuanle");
+        data.put("last_name", "tanh");
+        database.collection("users")
+                .add(data)
+                .addOnSuccessListener(documentReference -> {
+                    Toast.makeText(getApplicationContext(), "Data Inserted", Toast.LENGTH_SHORT).show();
+                })
+                .addOnFailureListener(exception -> {
+                    Toast.makeText(getApplicationContext(), exception.getMessage(), Toast.LENGTH_SHORT).show();
+                });
     }
 }
