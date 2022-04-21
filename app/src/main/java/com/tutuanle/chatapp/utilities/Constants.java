c2e7a89de175cb71b202cb8fd205eb5fac3a8b63
TuTuanLe <tutuanletkpt@gmail.com>
2022-04-21 22:51:39 +0700

    update sign up

--- app/src/main/java/com/tutuanle/chatapp/utilities/Constants.java
+++ app/src/main/java/com/tutuanle/chatapp/utilities/Constants.java
@@ -1,12 +1,13 @@
 package com.tutuanle.chatapp.utilities;
 
 public class Constants {
-    public  static  final  String KEY_COLLECTION_USERS = "users";
-    public  static final String KEY_NAME = "name";
+    public static final String KEY_COLLECTION_USERS = "users";
+    public static final String KEY_NAME = "name";
     public static final String KEY_EMAIL = "email";
-    public static  final String KEY_PREFERENCE_NAME= "ChatAppPreference";
+    public static final String KEY_PASSWORD = "password";
+    public static final String KEY_PREFERENCE_NAME = "ChatAppPreference";
     public static final String KEY_IS_SIGNED_IN = "isSignedIn";
-    public static final String KEY_USER_ID  = "userId";
-    public static final String KEY_IMAGE= "image";
+    public static final String KEY_USER_ID = "userId";
+    public static final String KEY_IMAGE = "image";
 
 }
