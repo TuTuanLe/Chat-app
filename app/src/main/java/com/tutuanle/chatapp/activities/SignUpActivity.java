78914c604e4e2288109922d84053f4bc098475ba
TuTuanLe <tutuanletkpt@gmail.com>
2022-04-21 17:29:51 +0700

    add encode Image

--- app/src/main/java/com/tutuanle/chatapp/activities/SignUpActivity.java
+++ app/src/main/java/com/tutuanle/chatapp/activities/SignUpActivity.java
@@ -2,13 +2,23 @@ package com.tutuanle.chatapp.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.util.Base64;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Toast;
 
 import com.tutuanle.chatapp.databinding.ActivitySignUpBinding;
+
+import java.io.ByteArrayOutputStream;
+
+import java.util.regex.Pattern;
 
 public class SignUpActivity extends AppCompatActivity {
 
     private ActivitySignUpBinding binding;
+    private String encodedImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,7 +28,71 @@ public class SignUpActivity extends AppCompatActivity {
         setListener();
 
     }
-    private void setListener(){
-        binding.textSignIn.setOnClickListener( view -> onBackPressed());
+
+    private void setListener() {
+        binding.textSignIn.setOnClickListener(view -> onBackPressed());
+
+        binding.buttonSignUp.setOnClickListener(v -> {
+            if (isValidSignUpDetail()) {
+                signUp();
+            }
+        });
+    }
+
+    private void showToast(String message) {
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+    }
+
+    private void signUp() {
+
+    }
+
+    private Boolean isValidSignUpDetail() {
+        if (encodedImage == null) {
+            showToast("Select Profile Iamge");
+            return false;
+        } else if (binding.inputName.getText().toString().trim().isEmpty()) {
+            showToast("Enter Email");
+            return false;
+        } else if (binding.inputEmail.getText().toString().trim().isEmpty()) {
+            showToast("Enter email");
+            return false;
+        } else if (!Patterns.EMAIL_ADDRESS.matcher(binding.inputEmail.getText().toString()).matches()) {
+            showToast("Enter valid Image");
+            return false;
+        } else if (binding.inputPassword.getText().toString().trim().isEmpty()) {
+            showToast("Enter the password");
+            return false;
+
+        } else if (binding.inputConfirmPassword.getText().toString().trim().isEmpty()) {
+            showToast("Confirm password");
+            return false;
+        } else if (!binding.inputPassword.getText().toString().equals(binding.inputConfirmPassword.getText().toString())) {
+            showToast("confirm password is not match");
+            return false;
+        }
+        return true;
+    }
+
+
+    private void loading(Boolean isLoading) {
+        if (isLoading) {
+            binding.buttonSignUp.setVisibility(View.INVISIBLE);
+            binding.progressBar.setVisibility(View.VISIBLE);
+
+        } else {
+            binding.buttonSignUp.setVisibility(View.VISIBLE);
+            binding.progressBar.setVisibility(View.INVISIBLE);
+        }
+    }
+
+    private String encodeImage(Bitmap bitmap) {
+        int previewWidth = 150;
+        int previewHeight = bitmap.getHeight() * previewWidth / bitmap.getWidth();
+        Bitmap previewBitmap = Bitmap.createScaledBitmap(bitmap, previewWidth, previewHeight, false);
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        previewBitmap.compress(Bitmap.CompressFormat.JPEG, 50, byteArrayOutputStream);
+        byte[] bytes = byteArrayOutputStream.toByteArray();
+        return Base64.encodeToString(bytes, Base64.DEFAULT);
     }
 }
