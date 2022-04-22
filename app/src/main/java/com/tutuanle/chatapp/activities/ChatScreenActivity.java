e4f8f43c9410fb927953c1e9c793db4c66ba8256
TuTuanLe <tutuanletkpt@gmail.com>
2022-04-22 22:46:09 +0700

    update

---
+++ app/src/main/java/com/tutuanle/chatapp/activities/ChatScreenActivity.java
@@ -0,0 +1,17 @@
+package com.tutuanle.chatapp.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.tutuanle.chatapp.databinding.ActivityChatScreenBinding;
+
+public class ChatScreenActivity extends AppCompatActivity {
+    private ActivityChatScreenBinding binding;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityChatScreenBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+    }
+}
