ecd9edc9599edec06e6dd0a0331bba2d87620d0d
pts ne <67184494+ptsne1002@users.noreply.github.com>
2022-05-29 01:47:45 +0700

    commit

--- app/src/main/java/com/tutuanle/chatapp/fragment/SettingFragment.java
+++ app/src/main/java/com/tutuanle/chatapp/fragment/SettingFragment.java
@@ -1,5 +1,6 @@
 package com.tutuanle.chatapp.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -16,6 +17,9 @@ import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.makeramen.roundedimageview.RoundedImageView;
 import com.tutuanle.chatapp.R;
 import com.tutuanle.chatapp.activities.MainScreenActivity;
+import com.tutuanle.chatapp.activities.ProfileActivity;
+import com.tutuanle.chatapp.models.User;
+import com.tutuanle.chatapp.utilities.Constants;
 
 
 public class SettingFragment extends Fragment {
@@ -25,6 +29,7 @@ public class SettingFragment extends Fragment {
     private static final String ARG_PARAM2 = "param2";
 
     private View view;
+    private User user;
 
     private MainScreenActivity mainScreenActivity;
     private GoogleSignInClient mGoogleSignInClient;
@@ -56,8 +61,32 @@ public class SettingFragment extends Fragment {
         mainScreenActivity =(MainScreenActivity) getActivity();
         initialData();
         setListeners();
+        openProfile();
         return view;
     }
+
+    private void openProfile() {
+
+        FrameLayout frameAccount = view.findViewById(R.id.frameAccount);
+
+        frameAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intenProfile = new Intent(getContext(), ProfileActivity.class);
+                user = new User();
+                user.setName(mainScreenActivity.preferenceManager.getString(Constants.KEY_NAME));
+                user.setUid(mainScreenActivity.preferenceManager.getString(Constants.KEY_USER_ID));
+                user.setProfileImage(mainScreenActivity.preferenceManager.getString(Constants.KEY_IMAGE));
+                user.setPhoneNumber(mainScreenActivity.preferenceManager.getString(Constants.KEY_NUMBER_PHONE));
+                user.setPassword(mainScreenActivity.preferenceManager.getString(Constants.KEY_PASSWORD));
+                user.setEmail(mainScreenActivity.preferenceManager.getString(Constants.KEY_EMAIL));
+                intenProfile.putExtra(Constants.KEY_USER,user);
+                startActivity(intenProfile);
+            }
+        });
+
+    }
+
 
     private void initialData() {
 
